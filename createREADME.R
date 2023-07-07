eb0fa76cf04a37a0895d8a3ef9b3605d9656e3c2
Spencer Williams <sjwilli6@ncsu.edu>
2023-07-07 10:09:47 -0400

    Create a render for ReadME

---
+++ createREADME.R
@@ -0,0 +1,17 @@
+# author: Spencer Williams and Stephen Macropoulos
+# date: 2023-07-09
+# purpose: Render Project2 as a .md file called README.md for our repo.
+
+rmarkdown::render(
+  input="ST558Project2",
+  output_format = "github_document",
+  output_file = "README.md",
+  runtime = "static",
+  clean = TRUE,
+  params = NULL,
+  knit_meta = NULL,
+  envir = parent.frame(),
+  run_pandoc = TRUE,
+  quiet = FALSE,
+  encoding = "UTF-8"
+)
